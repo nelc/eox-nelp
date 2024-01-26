18e5e69bbd61d5f04b0ac8c34586637d5ea006f3
Johan Castiblanco <johan.castiblanco@edunext.co>
2024-01-26 16:36:17 -0500

    feat: add PreserveUserLanguageCookieMiddleware
    This ensure to use the cookie language if the user send it.

--- eox_nelp/middleware.py
+++ eox_nelp/middleware.py
@@ -6,7 +6,7 @@ classes:
     ExtendedProfileFieldsMiddleware: Set extended_profile_fields in registration form.
 """
 from django.conf import settings
-from django.http import Http404
+from django.http import Http404, parse_cookie
 from django.utils.translation import gettext_lazy as _
 
 from eox_nelp.edxapp_wrapper.site_configuration import configuration_helpers
@@ -154,3 +154,21 @@ class TenantExistMiddleware:
             return self.get_response(request)
 
         raise Http404
+
+
+class PreserveUserLanguageCookieMiddleware:
+    """This middleware ensure that in the COOKIES property the LANGUAGE_COOKIE_NAME
+    key has to be the cookie sent by the user and not other,
+    because it could be modified previously by other.
+    eg.
+    https://github.com/openedx/edx-platform/blob/open-release/palm.master/openedx/
+    core/djangoapps/lang_pref/middleware.py#L61-L62
+    """
+    def process_request(self, request):
+        """Process the request to change the language cookie based in original cookie value."""
+        original_user_language_cookie = parse_cookie(request.META.get("HTTP_COOKIE", "")).get(
+            settings.LANGUAGE_COOKIE_NAME
+        )
+
+        if original_user_language_cookie:
+            request.COOKIES[settings.LANGUAGE_COOKIE_NAME] = original_user_language_cookie
