e0745e8e07b185969ca6d27709939ec41a7e1fb5
Johan Castiblanco <johan.castiblanco@edunext.co>
2025-09-12 09:36:10 -0500

    feat: add support for trailing slash

--- eox_nelp/programs/api/v1/urls.py
+++ eox_nelp/programs/api/v1/urls.py
@@ -7,5 +7,5 @@ from eox_nelp.programs.api.v1.views import ProgramsMetadataView
 app_name = "eox_nelp"  # pylint: disable=invalid-name
 
 urlpatterns = [
-    re_path(fr'metadata/{settings.COURSE_KEY_PATTERN}$', ProgramsMetadataView.as_view(), name='programs-metadata'),
+    re_path(fr'metadata/{settings.COURSE_KEY_PATTERN}/?$', ProgramsMetadataView.as_view(), name='programs-metadata'),
 ]
