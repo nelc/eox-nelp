24dfe1bbd6742ad8f3a2eefd2282346d173debae
Johan Castiblanco <johan.castiblanco@edunext.co>
2025-09-24 14:31:18 -0500

    feat: lazy transform of effort
    if effort format `HH:MM` is not interpretable, the function returns None
    and api should return 0
    
    chore: squqha

--- eox_nelp/programs/api/v1/utils.py
+++ eox_nelp/programs/api/v1/utils.py
@@ -77,7 +77,7 @@ def get_program_lookup_representation(course_api_data):
         "Type_of_Activity": TYPES_OF_ACTIVITY_MAPPING.get(program_metadata.get("Type_of_Activity", -1)),
         "Type_of_Activity_id": program_metadata.get("Type_of_Activity"),
         "Unit": "hour",
-        "duration": int(course_api_data.get("effort") or 0),
+        "duration": hms_to_int(course_api_data.get("effort")) or 0,
         "Mandatory": program_metadata.get("Mandatory"),
         "Program_ABROVE": program_metadata.get("Program_ABROVE"),
         "Code": course_api_data["course_id"],
@@ -105,3 +105,40 @@ def convert_to_isoformat(date_string):
     except ValueError as e:
         logger.error("Error parsing date string: %s", e)
         return None
+
+
+def hms_to_int(time_str):
+    """
+    Converts a time string to an integer representing total hours,
+    rounding to the nearest whole number. Handles both "HH:MM" and "HH" formats.
+
+    Args:
+        time_str: The time string (e.g., "2:30" or "2").
+
+    Returns:
+        An integer representing the total hours, rounded to the nearest whole number.
+        or None if input is invalid and there is some error.
+    """
+    if not time_str:
+        return None
+    if ":" in time_str:
+        try:
+            hours_str, minutes_str = time_str.split(":")
+            hours = int(hours_str)
+            minutes = int(minutes_str)
+
+            if minutes < 0 or minutes >= 60:
+                minutes = 0  # Reset invalid minutes to 0
+
+            # Calculate float and then round to the nearest integer
+            return round(hours + (minutes / 60))
+        except ValueError as e:
+            logger.error("Error converting time string: %s", e)
+            return None
+    else:
+        # If no colon, treat the string as whole hours
+        try:
+            return int(time_str)
+        except ValueError as e:
+            logger.error("Error converting time string: %s", e)
+            return None
